6d05bda2f60aae5509eb77a4ff3221a96db8afab
Sk Ferajuddin <officialferaj@gmail.com>
2024-09-24 18:23:01 +0530

    ⚡️ modular - home_page

--- lib/features/home/home_page.dart
+++ lib/features/home/home_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:gym_manager/features/home/widgets/home_gridcards.dart';
 import 'package:gym_manager/features/home/widgets/title_text.dart';
 import 'package:velocity_x/velocity_x.dart';
 
@@ -105,99 +105,11 @@ class _HomeViewState extends State<HomeView> {
                     BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20)),
               ),
               height: MediaQuery.of(context).size.height * 0.4,
-              child: GridView.builder(
-                physics: const AlwaysScrollableScrollPhysics(),
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  childAspectRatio: 1.6,
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 10,
-                  mainAxisSpacing: 10,
-                ),
-                itemCount: 4,
-                itemBuilder: (context, index) {
-                  return HomeCards(
-                    title: items[index]['title']!,
-                    icon: items[index]['icon']!,
-                  );
-                },
-              ).p12(),
+              child: HomeGridCards(items: items).p12(),
             ),
           ),
         ],
       ),
-    );
-  }
-}
-
-class HomeCards extends StatelessWidget {
-  final String title;
-  final IconData icon;
-  const HomeCards({
-    super.key,
-    required this.title,
-    required this.icon,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      color: Colors.amber.withOpacity(0.6),
-      // color: Colors.amber,
-      // color: Colors.white,
-      surfaceTintColor: Colors.white,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(15),
-      ),
-      elevation: 5,
-      child: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.6),
-                        offset: const Offset(0, 4),
-                        blurRadius: 10,
-                      ),
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.6),
-                        offset: const Offset(4, 0),
-                        blurRadius: 10,
-                      ),
-                    ],
-                    border: Border.all(
-                      width: 2,
-                      color: Colors.black,
-                    ),
-                    shape: BoxShape.circle),
-                child: Icon(icon),
-              ).h(45).w(45),
-              Text(title,
-                  overflow: TextOverflow.clip,
-                  style: TextStyle(
-                    fontSize: Theme.of(context).textTheme.labelMedium?.fontSize,
-                    fontFamily: GoogleFonts.poppins().fontFamily,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                    // color: Colors.amber,
-                  )),
-            ],
-          ),
-          Text('00',
-              overflow: TextOverflow.clip,
-              style: TextStyle(
-                fontSize: Theme.of(context).textTheme.headlineMedium?.fontSize,
-                fontFamily: GoogleFonts.poppins().fontFamily,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-                // color: Colors.amber,
-              )),
-        ],
-      ).p12(),
     );
   }
 }
---
+++ lib/features/home/widgets/home_gridcards.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+class HomeGridCards extends StatelessWidget {
+  const HomeGridCards({
+    super.key,
+    required this.items,
+  });
+
+  final List<Map<String, dynamic>> items;
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      physics: const AlwaysScrollableScrollPhysics(),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        childAspectRatio: 1.6,
+        crossAxisCount: 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+      itemCount: 4,
+      itemBuilder: (context, index) {
+        return HomeCards(
+          title: items[index]['title']!,
+          icon: items[index]['icon']!,
+        );
+      },
+    );
+  }
+}
+
+class HomeCards extends StatelessWidget {
+  final String title;
+  final IconData icon;
+  const HomeCards({
+    super.key,
+    required this.title,
+    required this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.amber.withOpacity(0.6),
+      // color: Colors.amber,
+      // color: Colors.white,
+      surfaceTintColor: Colors.white,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15),
+      ),
+      elevation: 5,
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.6),
+                        offset: const Offset(0, 4),
+                        blurRadius: 10,
+                      ),
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.6),
+                        offset: const Offset(4, 0),
+                        blurRadius: 10,
+                      ),
+                    ],
+                    border: Border.all(
+                      width: 2,
+                      color: Colors.black,
+                    ),
+                    shape: BoxShape.circle),
+                child: Icon(icon),
+              ).h(45).w(45),
+              Text(title,
+                  overflow: TextOverflow.clip,
+                  style: TextStyle(
+                    fontSize: Theme.of(context).textTheme.labelMedium?.fontSize,
+                    fontFamily: GoogleFonts.poppins().fontFamily,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                    // color: Colors.amber,
+                  )),
+            ],
+          ),
+          Text('00',
+              overflow: TextOverflow.clip,
+              style: TextStyle(
+                fontSize: Theme.of(context).textTheme.headlineMedium?.fontSize,
+                fontFamily: GoogleFonts.poppins().fontFamily,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+                // color: Colors.amber,
+              )),
+        ],
+      ).p12(),
+    );
+  }
+}
